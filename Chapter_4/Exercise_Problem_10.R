9e548e37657eab1059679ffd96b7a04e7ebd4963
Venkata K Nunna <NUNNAVK@asia.ad.whirlpool.com>
2018-12-04 19:06:07 +0530

    Chapter 4 , 10th problem is completed

--- Chapter_4/Exercise_Problem_10.R
+++ Chapter_4/Exercise_Problem_10.R
@@ -35,6 +35,9 @@ summary(glm_model)
 
 ## c)
 # Plotting the confusion matrix using table function
+# Here we have to include type = "response" , otherwise instead of probabilities it will return log odds
+# log odds are log of ratio of success to failure.
+# Say logodds(Going Up) =log( (ProbofUP)/(ProbofDown) )
 glm_preds = predict(object = glm_model,newdata = weekdata,type = "response")
 glm_preds
 # ifelse function will take dataframe as first argument and then if its passed then it will give second argument as yes and if its false then third argument is applied .
@@ -93,3 +96,143 @@ glmPredsAfter2008 = ifelse(glmPredsAfter2008 > 0.5, "Up","Down")
 
 # Checking with table,
 table(glmPredsAfter2008,DirectionAfter2008)
+
+# % of correct predictions are
+mean(glmPredsAfter2008 == DirectionAfter2008)*100
+
+## e)
+# Now using LDA to predict
+# lda is part of mass library, so loading it
+library(MASS)
+
+lda_fit = lda(Direction ~ Lag2,data = weekdataUntil2008)
+lda_fit
+
+# Our lda predictions as discussed earlier is list of 3, class, posteriorprob and LD1
+lda_preds = predict(object = lda_fit,newdata = weekdataAfter2008)
+
+# confusion matrix
+table(lda_preds[["class"]],DirectionAfter2008)
+# We got same confusion matrix as logistic regression
+
+# % of correct predictions are
+mean(lda_preds[["class"]] == DirectionAfter2008)*100
+
+## f)
+# Now using QDA to predict
+qda_fit = qda(Direction~Lag2,data = weekdataUntil2008)
+qda_fit
+
+# predicting on data after 2008
+qda_preds = predict(object = qda_fit,newdata = weekdataAfter2008)
+qda_preds
+
+# Confusion matrix
+table(qda_preds[["class"]],DirectionAfter2008)
+# % of correct predictions are
+mean(qda_preds[["class"]]==DirectionAfter2008)*100
+
+
+## g)
+# Now using KNN to predict
+# KNN is part of class library, so importing that first
+library(class)
+
+# If you dont modify the dataframe when using single variable the dimension of it throws error. So its better to always make new dataframe using cbind or as.matrix
+# Then use that on the splitted data.
+# No need to modify response data in cl, just give that as a column of dataframe
+# Column of dataframe has null dimension
+# cbind() ones have dimension as like matrix
+# Dataframe has dimension, but not column of dataframe.
+knn_fit = knn(train = cbind(weekdataUntil2008$Lag2)
+              ,test = cbind(weekdataAfter2008$Lag2)
+              , cl =weekdataUntil2008$Direction,k = 1)
+knn_fit
+
+# Confusion matrix
+table(knn_fit,DirectionAfter2008)
+
+# % of correct predicitons are
+mean(knn_fit == DirectionAfter2008)*100
+
+## h)
+## Of all the models knn-50% , qda-58% ,glm - 62% , lda - 62.5%
+# So lda and logistic regression are better to fit.
+
+
+## i) Now to experiment using different variables, and transformations as well
+## Considering Lag1 along with Lag2
+
+
+glm.fit = glm(Direction ~ Lag2+Lag2^2,family = binomial,data = weekdataUntil2008)
+glm.fit
+glm.pred = predict(glm.fit,newdata = weekdataAfter2008,type = "response")
+glm.pred
+glm.pred = ifelse(glm.pred > 0.5, "Up","Down")
+mean(glm.pred == DirectionAfter2008) # Gives 62.5% correct
+
+
+
+glm.fit = glm(Direction ~ Lag2+log(abs(Lag2)),family = binomial,data = weekdataUntil2008)
+glm.fit
+glm.pred = predict(glm.fit,newdata = weekdataAfter2008,type = "response")
+glm.pred
+glm.pred = ifelse(glm.pred > 0.5, "Up","Down")
+mean(glm.pred == DirectionAfter2008) # Gives 62.5% correct
+
+
+glm.fit = glm(Direction ~ Lag2+Lag2^2+Lag^3,family = binomial,data = weekdataUntil2008)
+glm.fit
+glm.pred = predict(glm.fit,newdata = weekdataAfter2008,type = "response")
+glm.pred
+glm.pred = ifelse(glm.pred > 0.5, "Up","Down")
+mean(glm.pred == DirectionAfter2008) # No improvement, same 62.5%
+
+
+lda.fit = lda(Direction ~ Lag2+Lag2^2,data = weekdataUntil2008)
+lda.fit
+lda.pred = predict(lda.fit,newdata = weekdataAfter2008)
+lda.pred # list of 3;
+lda.classes = lda.pred[["class"]]
+mean(lda.classes == DirectionAfter2008)*100 # No improvement, same 62.5%
+
+# Increasing the k neighbours in knn to 10
+knn.fit = knn(train = cbind(weekdataUntil2008$Lag2),test = cbind(weekdataAfter2008$Lag2),cl = DirectionUntil2008,k = 10)
+knn.fit
+mean(knn.fit == DirectionAfter2008)*100   # 55%
+
+# knn neighbours to 30
+knn.fit = knn(train = cbind(weekdataUntil2008$Lag2),test = cbind(weekdataAfter2008$Lag2),cl = DirectionUntil2008,k = 30)
+knn.fit
+mean(knn.fit == DirectionAfter2008)*100 # 51%
+
+# knn neighbours to 25
+knn.fit = knn(train = cbind(weekdataUntil2008$Lag2),test = cbind(weekdataAfter2008$Lag2),cl = DirectionUntil2008,k = 25)
+knn.fit
+mean(knn.fit == DirectionAfter2008)*100 # 53%
+
+# knn neighbours to 20
+knn.fit = knn(train = cbind(weekdataUntil2008$Lag2),test = cbind(weekdataAfter2008$Lag2),cl = DirectionUntil2008,k = 20)
+knn.fit
+mean(knn.fit == DirectionAfter2008)*100 # 58%
+
+# knn neighbours to 17
+knn.fit = knn(train = cbind(weekdataUntil2008$Lag2),test = cbind(weekdataAfter2008$Lag2),cl = DirectionUntil2008,k = 17)
+knn.fit
+mean(knn.fit == DirectionAfter2008)*100 # 59%
+
+
+# knn neighbours to 16
+knn.fit = knn(train = cbind(weekdataUntil2008$Lag2),test = cbind(weekdataAfter2008$Lag2),cl = DirectionUntil2008,k = 16)
+knn.fit
+mean(knn.fit == DirectionAfter2008)*100 # 56%
+
+
+# knn neighbours to 15
+knn.fit = knn(train = cbind(weekdataUntil2008$Lag2),test = cbind(weekdataAfter2008$Lag2),cl = DirectionUntil2008,k = 15)
+knn.fit
+mean(knn.fit == DirectionAfter2008)*100 # 58%
+
+# So optimized knn is between 15 to 20 and max lies for around k = 17-18
+# Lda and glm with lag^2 as variable gives highest result
+
