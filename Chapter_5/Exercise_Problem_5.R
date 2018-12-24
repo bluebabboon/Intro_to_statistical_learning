0fcfc760ffc0f6fad1d39a0fa939a1e7bcdafd69
Krishna Praveen <nssvkpraveen@gmail.com>
2018-12-24 10:25:27 +0530

    Update Exercise_Problem_5.R

--- Chapter_5/Exercise_Problem_5.R
+++ Chapter_5/Exercise_Problem_5.R
@@ -11,4 +11,24 @@ glmmodel
 
 summary(glmmodel)
 
+# Splitting the model into training and testing data
+names(defaultdata)
+dim(defaultdata)
+nrow(defaultdata)
 
+
+# Using the sample function to select random indexes
+?sample
+train = sample(x = nrow(defaultdata),size = 0.7*nrow(defaultdata))
+train
+length(train)
+
+# Now we have selected the indexes that we are going to use for the training the data in the train
+# All other data is going to be used for testing the trained algorithm
+
+
+# Now we are going to fit the default data with other factors that are student balance and income using multiple logistic regression model
+lm.fit = glm(formula = default~student+income+balance,family = binomial,data = defaultdata,subset = train)
+lm.fit
+
+# Lets predict on all the dataset
