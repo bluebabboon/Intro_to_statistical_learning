d5c4dcf2de598433e9af8e3ac6fb9f7c74f0f619
krishna-praveen <nssvkpraveen@gmail.com>
2018-12-26 10:45:20 +0530

    Completed Problem 7

--- Chapter_5/Exercise_Problem_7.R
+++ Chapter_5/Exercise_Problem_7.R
@@ -20,101 +20,39 @@ lmfit_not1 = glm(formula = Direction~Lag1+Lag2,family = binomial,data = weekdata
 lmfit_not1
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+## c)
+# Now using the model from b to predict the direction of rist observation
+firstpred = predict(object = lmfit_not1,newdata = weekdata[1,],type = "response")
+firstpred
+firstpred_categorical = ifelse(firstpred > 0.5,"Up","Down")
+firstpred_categorical
+# Now checking if it is correct by comparing it with the actual correct observation
+Direction[1] == firstpred_categorical
+# It was not correctly classified
+
+## d)
+# Now creating a function that does the following
+# Fits a logistic regression in a forloop from 1 to nrow(data)
+# Where using all but ith observation in that ith loop
+# Creating a empty array of same dimesion as weekdata to store the errors, if occured
+
+error_array = rep(x = 0,nrow(weekdata))
+error_array
+
+for (i in 1:nrow(weekdata)) {
+  glmmodel = glm(formula = Direction~Lag1+Lag2,family = binomial,data = weekdata[-i,])
+  predhere = predict(object = glmmodel,newdata = weekdata[i,],type = "response")
+  predhere_categorical = ifelse(predhere > 0.5,"Up","Down")
+  if (Direction[i] == predhere_categorical) {
+    NULL
+  }else{
+    error_array[i]=1
+  }
+}
+
+error_array
+sum(error_array)
+mean(error_array)
+
+# So total 44.9% of the observations are misclassified if using LOOCV estimation methodology
 
