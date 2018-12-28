0613bec89d1b7e5d8366af9bfcf77db973aa5205
krishna-praveen <nssvkpraveen@gmail.com>
2018-12-28 19:40:49 +0530

    Updated Lab Chapter 6

--- Chapter_6/Lab_Chapter_6.R
+++ Chapter_6/Lab_Chapter_6.R
@@ -70,6 +70,91 @@ names(regfitfullsummary)
 # Lets see what is the Rsquared value for each subset we have calculated
 regfitfullsummary$rsq
 
+# As expected R^2 statisti increaes as we include more variables.
+# Its better we plot these R^2 statistic instead of just verifying in th tcommand line
+
+# Splitting the Gui in to 2x2 gui scheme
+par(mfrow=c(2,2))
+
+?plot
+
+# Using the "l" inside plot function with type will let us plot a line graph instead of dotted scatter plot
+# Plotting the RSS with number of variables
+plot(regfitfullsummary$rss,xlab = "Number of variables",ylab = "RSS",type = "l")
+
+# Plotting the Adjustted R^2 with number of variables
+plot(regfitfullsummary$adjr2,xlab = "Number of variables",ylab = "Adjusted R^2",type = "l")
+
+
+# Another function to the rescue is point function which works like the plot command ,except that it puts points on a plot that has already been created, insted of creating a new plot
+# which.max() is another function which can be used to identify the locaiton of maximum point of a vector.We will see which is the max point in the vector of values we  have plotted and then use that index to use it inisde the points() function.
+#
+which.max(regfitfullsummary$adjr2)
+
+# It says that 11th index has the maximum value
+
+# Now using the points function to plot the red dot on the existing plot
+?points
+
+# The arguments inside the points function are
+# First argument is the index where the modification of datapoint's apperance takes place to be
+# Second argument is the dataset that we are using and on which the indexes value has to be derived from
+# Actually the first 2 arguments here means that x and y coordinate on the plot which we want to plot on
+# And this function will plot on the current plot that is activated. Now the second one is activated and it will use that as reference until we use another plot function
+# col is just argument that gives colour to the point
+# cex is the argument that increases the dot size that we are plotting. Giving 2 value is the best one.
+# pch is the plotting character, here 20 stands for circle, we can also have square or various other things.
+points(11,regfitfullsummary$adjr2[11],col="red",cex=2,pch=20)
+
+
+# In a similar fashion we can plot the Cp, and BIC and indicate the models which has the smallest statistic using which.min()
+# Using line plot
+plot(regfitfullsummary$cp,xlab = "Number of predictors",ylab = "Cp value",type = 'l')
+# Finding the minimum of the cp values
+which.min(regfitfullsummary$cp)
+# 10th one is the minimum index, Now using points function to plot this point on the currently activated grpah that is nothing but the cp graph
+points(10,regfitfullsummary$cp[10],col="green",cex=2,pch=20)
+
+
+# Now plotting the Bic the sameway we have dont for the Cp
+plot(regfitfullsummary$bic,xlab = "Number of predictors",ylab = "Bic value",type = "l")
+# Finding the minimum of the bic
+which.min(regfitfullsummary$bic)
+# Minimum one is 6, so using this 6 index on the current plot to plot the point plot
+points(6,regfitfullsummary$bic[6],col="magenta",cex=2,pch=20)
+
+
+
+#####
+## Until now we are using the summary of the fitted model that is from the regsubsets
+## We can use the actual output of the regsubests instead of summary to use the built in plot command which can be used to display the selected variables
+
+par(mfrow=c(2,2))
+
+# Help to know more about the regsubsets plot
+?plot.regsubsets
+
+# Here it plots a plot which has variables on the x axis and what ever the criteron on the Y axis
+# The plot shows a white and black plot
+# It can be interpreted in the following way. For a given Y value and if we are checking for a particular R^2 value, we see the model that it gives that R^2
+# In that model if we go along the X axis (Draw a line which is parellel to X axis at that particular Y value), then if a particular variable is present in that model we see a "BLACK" colored square on the plot.
+# If that particular variable is not present , then it will be "WHITE" colored square
+plot(regfitfull,scale = "r2")
+plot(regfitfull,scale = "adjr2")
+plot(regfitfull,scale = "Cp")
+plot(regfitfull,scale = "bic")
+
+# Check the above plot and in that for bic plot , we see that we have several models share a BIC close to -15-, However the model with lowest BIC (Lowest means higher negative value), has only 6 variables
+# They are AtBat,Hits,Walks,CRBI,DivisionW,PutOuts
+# We can use the coef() function to see the coefficeint estimates associated with this model
+
+coef(regfitfull,19)
+
+
+
+
+
+
 
 
 
