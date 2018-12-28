542578072b3cba709b9cbe5a797f8859af64925b
Krishna Praveen <nssvkpraveen@gmail.com>
2018-12-28 12:54:54 +0530

    Lab chapter 6 updated

--- Chapter_6/Lab_Chapter_6.R
+++ Chapter_6/Lab_Chapter_6.R
@@ -36,6 +36,54 @@ sum(is.na(x = hitterdata))
 
 ###
 ## Lets use the library called leaps which does the best subset selection by identifying the best model that contaisn a given number of predictors , where best is quantiied using RSS
+install.packages("leaps")
+??leaps
+library(leaps)
+?regsubsets
+
+# We are going to use regsubsets function for model selection.
+# It scans through all the subsets of variables using best subset selection model method
+# And then gives output if we call summary for each number of predictors which are relevant
+# That means for each number of predictors that we want to fit the model with, it will tell what are best predictors that we can use to predict in best way using RSS
+
+regfit = leaps::regsubsets(hitterdata$Salary~.,hitterdata)
+
+summary(regfit)
+# In summary we can see that it keeps a star and we have column of numbers
+# This numbers indicates the number of variables that the function is considering
+# By default regsubset only include model upto 8 variables.
+# But we can add more by providing an argument called "nvmax"
+
+# Now using all the variables in the data
+dim(hitterdata)
+
+# We have 20 dimensions and we are fitting salary, so we need to use the rest 19 variables
+regfitfull = regsubsets(x = hitterdata$Salary~. , data = hitterdata,nvmax = 19)
+summary(regfitfull)
+
+regfitfullsummary = summary(regfitfull)
+
+names(regfitfullsummary)
+
+# We can see that inside the summary, we have also R^2, RSS , adjusted R^2, Cp and BIC
+# We can use these to try and select the best overall model
+# Lets see what is the Rsquared value for each subset we have calculated
+regfitfullsummary$rsq
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
