b3e9917118e8ecea68c15fd34a9e17c6b1cf9ae0
krishna-praveen <nssvkpraveen@gmail.com>
2019-01-17 20:26:27 +0530

    updated chapter 7 lab

--- Chapter_7/Lab_Chapter_7.R
+++ Chapter_7/Lab_Chapter_7.R
@@ -42,21 +42,51 @@ coef(summary(fit2a))
 # So indirectly using the argument "raw = T" means using the I() function for each of the degree
 #   until the given number of degrees are specified.
 
+# Another type of fit is using the age , age^2 .. age^n , by creating them and then binding
+#   them using cbind
+fit2b = lm(wage~cbind(age, age^2, age^3, age^4),data = wagedata)
+summary(fit2b)
+coef(summary(fit2b))
+
+# Both using the I(age) and just using the cbind is the same way of fitting the data and the
+#   coefficients we got are same form both the different models.
 
 
+# We shall now create a grid of values for which we want to predict for different values of
+#   ages. We shall then use the predict() function to call
 
+?range
+# Range returns the minimum and maximum of all the given arguments iniside it
+agelims = range(age)
 
+age_grid = seq(agelims[1],agelims[2])
 
+# Predicting for the grid values we have created, here in the new data we have given the data
+#   input as list() and in the list we are saying that the column name to be age and takes
+#   all the age_grid values.
+# Also we have another argument called se.fit which will let us output all the standard errors
+#   that we need for each value of age_grid data that we see.
+#
+preds = predict(object = fit,newdata = list(age = age_grid),se.fit = TRUE)
 
+# Now you can see that preds is object like structure with different columns that it has
+#   One of that is se.fit which holds all the standard errors
+preds
 
+# Now  we are creating the standard error bands with + or - two times the standard errors
+#   to create a band in which our predictions lie for each grid value in the age_grid
+se.bands = cbind(preds$fit + 2*preds$se.fit , preds$fit - 2*preds$fit)
 
-
-
-
-
-
-
-
+# Lets plot those standard errors along with the predictions that it has made
+?par
+# par is used to set or query graphical parameters. They can also be set
+#   mfrow will split the graph display portion in to number of splits ,where each graph is one
+#     split.
+#   mar is used to set margin c(bottom, left, top,right) , a vector of these 4 values to be
+#     given to set the margin space
+#   oma is outer margin, this is for the entire grpah displaying portion itself while the previous
+#     one is for each plot alone. follows the same c(bottom, left, top , right)
+par(mfrow = c(1,2),mar = c(4.5,4.5,1,1),oma = c(0,0,4,0))
 
 
 
