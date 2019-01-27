4ccdf2edc0a2b37109e1db54b84aa80a32e19810
Krishna Praveen <nssvkpraveen@gmail.com>
2019-01-28 02:16:58 +0530

    updated exercise problem 6 of chapter 7

--- Chapter_7/Exercise_Problem_6.R
+++ Chapter_7/Exercise_Problem_6.R
@@ -59,28 +59,62 @@ model_fit_list = list()
 model_fit_names = paste0("fit",1:10)
 
 for (i in 1:10) {
-  assign(paste("fit",i,sep = ""),i)
+
+  # Creating a current linear lm model with lm function
+  current_model = lm(wage~poly(age,i),data = wagedata)
+
+  # Now we have to assign this current model to the variable name that we have created in the
+  #   model fit names and for that current name we have use assign function
+
+  # Here we are assigning the values for those there in the first argument of assign to the seoncd
+  #   argument in the assign function
+  # In the first argument we have the fit0, or fiti based on the ith variable that we are currently in
+  #   the loop, And in the second argument we should have the value that we want to assign to the first
+  #   argument as the variable name
+  # assign(paste("fit",i,sep = ""),i^^)
+
+  assign(model_fit_names[i],current_model)
+
 }
 
 
-arbit = paste0("a",1:10)
+# Now that we have fit all the 10 models in fit1, fit2 ... fit10 respectievely ,now we have to do the
+#   anvoa on this
+?anova
+anova(fit1,fit2,fit3,fit4,fit5,fit6,fit7,fit8,fit9,fit10)
+
+# Anova says that adding the 2nd degree to the fit will incerase the F value , and then adding another
+#   3rd variable have nice significance. But adding the 4th one is not giving any value addition
 
 
 
 
+### b)
+# Now to fit a step function to predict the wage using age and then perform cross validation  to choose
+#   the optimal number of cuts. Also then to obtain the plot
+# We can use the cut function to fit the categorical type of variable to our data
+# Cut function will basically conver the numeric data into factor variable.
+?cut
+table(cut(age,4))
 
+# We are plotting from the table , for the cut(age,4) which will give the grouped type of thing
+#   where each factor will be shown how many number of times it is appeared in the data after
+#   it has been converted in to the factor type.
+step_fit = lm(wage~cut(age,4),data = wagedata)
+coef(step_fit)
+summary(step_fit  )
 
+# From the above sumamry it says that the 4th cut is not that significant.
 
+# Now we want to choose the optimal number of cuts , but we are going to iterate from 1 to 10 number
+#   of cuts and for each cut we are going to save the error and see which error is the lowest
 
+step_cv_errors = rep(0,10)
 
-
-
-
-
-
-
-
-
+for (i in 2:10) {
+  step_glm_fit =  glm(wage~cut(age,i),data = wagedata)
+  step_cv_errors[i] = cv.glm(glmfit = step_glm_fit,data = wagedata,K = 10)$delta[1]
+}
 
 
 
