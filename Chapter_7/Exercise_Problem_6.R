dced6700fb6aa6da378e2892fe2940ad6cecf35a
krishna-praveen <nssvkpraveen@gmail.com>
2019-01-28 18:00:22 +0530

    completed problem 6

--- Chapter_7/Exercise_Problem_6.R
+++ Chapter_7/Exercise_Problem_6.R
@@ -62,10 +62,33 @@ model_fit_names = paste0("fit",1:10)
 ?assign
 
 
+
+#########################
+##### USE THIS URL FOR THE NEW CONCEPT OR ERROR WE HAVE LEARNT
+## http://r.789695.n4.nabble.com/Predict-polynomial-problem-td1017140.html
+
+# We have learnt a new concept of creating models multiple times and then saving them inside a
+#   list, Where we can call the different models in the list and use predictions on them
+#   whenever we want
+#
+
+# bquote is a function that will help us in letting our predict function take correct i value
+#   and then give correct predictions , rather than relying on the global i value that is stored
+#   in the environment.
+?bquote
+
 for (i in 1:10) {
 
   # Creating a current linear lm model with lm function
-  current_model = lm(wage~poly(age,i),data = wagedata)
+  # The following formula will not be able to give predictions if we use predictions other
+  #   than fit10 in the predict function. Because the current i inside the predict function will
+  #   10 at the end of loop and the formula will try to use 10, but the model we are supplying
+  #   if it is other than fit10, then it will throw the error
+  #
+  ####  current_model = lm(wage~poly(age,i),data = wagedata)
+
+  # So we have to use the following formula instead of the formula we have above
+  current_model = lm(bquote(wage~poly(age,.(i))),data = wagedata)
 
   # Now we have to assign this current model to the variable name that we have created in the
   #   model fit names and for that current name we have use assign function
@@ -95,9 +118,50 @@ age_limits = range(wagedata$age)
 
 age_grid = seq(age_limits[1],age_limits[2])
 
-fit10 = lm(wage~poly(age,10),data = wagedata)
+# Just calling preds4 in this example without modifying the formula we are specifying in the
+#   loop of model we have saved will not work
+# Here in the below predictions we are doing, the predict function first argument is model
+#   And the model we are giving it is fit4, And by common sense it has to use fit4 and use the
+#   poly(age,4) inside the formula, But what happens is that the formula we have given is
+#   poly(age,i) and the current i will be the last i in the loop we have given and it is 10
+#   So poly is doing poly(age,10) and the model only has 4, So it will throw this error
+# TO fix this error we have to do this
+#
+# Instead of using lm(wage~poly(age,i)) in the formula in the for loop above we have to do this
+# lm(wage~bquote(wage~poly(age,.(i))))
+#
+# To learn why is this happening is because as per bquote , bquote quotes its argument except that
+#   the terms wrapped in the ".()" are evaluated in specified "where" environment and that is
+#   its parent.frame by default.
+preds4 = predict(fit4,newdata = list(age=age_grid),se.fit = TRUE)
 
-preds4 = predict(fit3,newdata = list(age=age_grid),se.fit = TRUE)
+# Creating the bands of standard errors with +- 2 times the preds4$fit
+# Binding the plus and minus errors from its main , in columnwise
+standarderror_bands_4 = cbind(preds4$fit + 2*preds4$se.fit , preds4$fit - 2*preds4$se.fit)
+
+plot(age,wage,xlim = age_limits,cex = 0.5,col = "darkgrey")
+
+# plotting the lines for each value of grid as x value in age_grid and corresponding predicted
+#   value on y. With lwd giving the line width , col giving colur
+?lines
+lines(age_grid,preds4$fit,lwd = 3, col = "green")
+
+# matlines to plot one column as x and keeping the same x but different columns of y of another
+#   dataframe, or list or whatever. And then plotting the lines, And lwd with line width
+#   and lty giving the line type. lty 3 will be dashed line type
+?matlines
+matlines(age_grid, standarderror_bands_4, lwd = 0.5, lty = 3, col = "red")
+
+# Finally giving the title to the plot
+title("4th Degree polynomial fit")
+
+
+
+
+
+
+
+
 
 
 
@@ -153,21 +217,20 @@ plot(2:10,step_cv_errors,type = "b")
 table(wagedata$cut_age)
 
 
+# Plotting the step_glm_fit for 8 number of cuts, which is 7th index
+step_glm_fit_8 = glm(wage~cut(age,8),data = wagedata)
 
+preds8 = predict(object = step_glm_fit_8,newdata = list(age = age_grid),se.fit = TRUE)
 
+standarderror_bands_8 = cbind(preds8$fit + 2*preds8$se.fit , preds8$fit - 2*preds8$se.fit)
 
+plot(age,wage,col = "darkgrey", xlim = age_limits,cex = 0.5)
 
+lines(age_grid,preds8$fit,lwd = 2, col = "green")
 
+matlines(age_grid,standarderror_bands_8,lty = 3, lwd = 0.5, col = "red")
 
-
-
-
-
-
-
-
-
-
+title("Step fit for 8 number of cuts")
 
 
 
