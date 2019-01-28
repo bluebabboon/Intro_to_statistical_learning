8ad24a84f6c9b411c8e8861410425985e581a46c
krishna-praveen <nssvkpraveen@gmail.com>
2019-01-28 19:59:59 +0530

    updated chapter 7 problem 7

---
+++ Chapter_7/Exercise_Problem_7.R
@@ -0,0 +1,5 @@
+# In the wage dataset we have a number of features that are not explroed
+# We can explore the relationship between some of these other predictors and wage
+#   And then use non linear fitting techniques in order to fit fliexibel models to data
+#
+# And then plot the results obtained and then finally write a summaryh of the findings
