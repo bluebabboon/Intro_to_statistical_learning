5edb37c99a51d44e063f04df541e5b5be2c7e72c
Krishna Praveen <nssvkpraveen@gmail.com>
2019-02-09 12:18:20 +0530

    updated chapter 8 problem 10

--- Chapter_8/Exercise_Problem_10.R
+++ Chapter_8/Exercise_Problem_10.R
@@ -8,6 +8,8 @@ rm(list = ls())
 library(ISLR)
 hitterdata = Hitters
 
+names(hitterdata)
+
 sum(is.na(hitterdata))
 
 hitterdata = na.omit(hitterdata)
@@ -16,7 +18,7 @@ sum(is.na(hitterdata))
 
 dim(hitterdata)
 
-hitterdata["logSalaries"] = log(hitterdata$Salary)
+hitterdata["Salary"] = log(hitterdata$Salary)
 
 dim(hitterdata)
 
@@ -27,7 +29,7 @@ head(hitterdata)
 # Creating a training set with first 200 observations and the test set
 #   will be rest of the remaining observations
 
-triandata = hitterdata[1:200,]
+traindata = hitterdata[1:200,]
 testdata = hitterdata[-(1:200),]
 
 
@@ -36,32 +38,70 @@ testdata = hitterdata[-(1:200),]
 #   shrinkage parameter lambda. Then give a plot with different shrinkage values on the
 #   x axis and the corresponding training set MSE on the y axis
 
+
+
 library(gbm)
 
 ?gbm
 
-boost_hittermodel = gbm()
+# We need dataset without salaries.So lets create another test and train without salary
 
+set.seed(42)
 
+names(traindata)
 
+boost_hittermodel = gbm(Salary ~ ., data = traindata,
+                        distribution = "gaussian", n.trees = 1000, interaction.depth = 4)
 
+summary(boost_hittermodel)
 
+plot(boost_hittermodel)
 
+boost_hittermodel$shrinkage
 
+preds_boost = predict(object = boost_hittermodel,newdata = traindata,n.trees = 1000)
 
+mean((preds_boost - traindata$Salary)^2)
 
+# Lets create a shrinkage vector starting from 1000 to 0.001 or something like that
 
+shrinkage_vecs = 10^seq(-10,-0.1,by = 0.1)
 
+shrinkage_vecs[-20:0]
 
+training_mse = rep(0,length(shrinkage_vecs))
 
+test_mse = rep(0,length(shrinkage_vecs))
 
+# For each of these shrinkage value we have to create a model and then get its predictions and
+#   then calculate the MSE of that training set
 
+for (i in 1:length(shrinkage_vecs)) {
+  boost_model =  gbm(Salary ~ ., data = traindata,
+                        distribution = "gaussian", n.trees = 1000,
+                        interaction.depth = 4, shrinkage = shrinkage_vecs[i])
 
+  boost_preds_train = predict(boost_model, newdata = traindata, n.trees = 1000)
 
+  boost_preds_test = predict(boost_model, newdata = testdata, n.trees = 1000)
 
+  training_mse[i] = mean((boost_preds_train - traindata$Salary)^2)
 
+  test_mse[i] = mean((boost_preds_test - traindata$Salary)^2)
 
+}
 
+par(mfrow = c(1,2))
+
+plot(training_mse)
+
+plot(test_mse)
+
+which.min(test_mse)
+
+test_mse[1]
+
+shrinkage_vecs[which.min(test_mse)]
 
 
 
